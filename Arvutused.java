250435d6813da5229019e9da9571840abaf8a930
Martin Holtsmeier <MartinHolts@users.noreply.github.com>
2017-05-04 09:38:15 +0300

    Update Arvutused.java

--- Arvutused.java
+++ Arvutused.java
@@ -17,5 +17,10 @@ public class Arvutused {
 		obj.muudaKorgusVaartus(5);
 		obj2.muudaVastust(obj.tagastaKaatet1Vaartus() * obj.tagastaKorguseVaartus() / 2);
 		System.out.println("Antud taisnurkse kolmnurga pindala on " + obj2.annaVastus() + " Uhikut.");
+		obj.muudaKaatet1Vaartus(10);
+		obj.muudaKorgusVaartus(10);
+		obj2.muudaVastust(obj.tagastaKaatet1Vaartus() * obj.tagastaKorguseVaartus() / 2);
+		System.out.println("Antud taisnurkse kolmnurga pindala on " + obj2.annaVastus() + " Uhikut.");
+		
 	}
 }
